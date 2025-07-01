16e194e77f43fe4d3a165746d9417c19b95c3243
jnikita19 <Nikita.Joshi@mygurukulam.co>
2025-07-01 17:29:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,40 +1,34 @@
-resource "aws_lb_target_group" "this" {
-  name        = "${var.application_name}-tg"
-  port        = var.application_port
-  protocol    = var.tg_protocol
+resource "aws_lb_target_group" "target_group" {
+  name        = "${var.applicaton_name}-alb-tg"
+  port        = var.applicaton_port
   target_type = var.tg_target_type
+  protocol    = var.tg_protocol
   vpc_id      = var.vpc_id
-
   health_check {
-    path = var.health_check_path
-  }
-
-  tags = {
-    Name             = "${var.application_name}-tg"
-    LoadBalancerARN  = var.load_balancer_arn != "" ? var.load_balancer_arn : "null"
+    path = var.applicaton_health_check_target
   }
 }
-
-resource "aws_lb_target_group_attachment" "attachment" {
-  count            = var.enable_attachment ? length(var.instance_ids) : 0
-  target_group_arn = aws_lb_target_group.this.arn
-  target_id        = var.instance_ids[count.index]
-  port             = var.application_port
+resource "aws_lb_target_group_attachment" "target_group_inctance" {
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = var.instance_id
+  port             = var.applicaton_port
 }
 
-resource "aws_lb_listener_rule" "this" {
-  count        = var.add_listener_rule ? 1 : 0
+# Conditionally create an ALB listener rule
+resource "aws_lb_listener_rule" "alb_listener_rule" {
+  count = var.add_listener_rule ? 1 : 0
+
   listener_arn = var.listener_arn
   priority     = var.listener_rule_priority
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.this.arn
+    target_group_arn = aws_lb_target_group.target_group.arn
   }
 
   condition {
-    host_header {
-      values = var.listener_rule_host_headers
+    path_pattern {
+      values = ["/"]
     }
   }
 }
